32b70290ddb248a34f70bc588de3da38e7fef272
Hira3th <cedric-98@hotmail.co.uk>
2022-01-24 23:13:36 +0100

    test
    gfds

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/market'));
 
+app.all('/', function(req, res, next) {
+    res.header("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
+    next();
+  });
+
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/market/'}),
 );
--- src/app/token-interceptor.service.ts
+++ src/app/token-interceptor.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, Injector } from '@angular/core';
-import { HttpInterceptor } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { AuthService } from './auth.service';
+import { Observable, throwError } from 'rxjs';
+import { catchError, finalize, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +11,31 @@ export class TokenInterceptorService implements HttpInterceptor {
 
   constructor(private _injector: Injector) { }
 
-  intercept(req: any, next: any){
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>
+  {
     let authService = this._injector.get(AuthService)
-    let tokenizedReq = req.clone({
+    req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${authService.getToken()}`
       }
     })
-    return next.handle(tokenizedReq)
-  }
+    return next.handle(req)
+    .pipe(
+      // Retry on failure
+      retry(2),
+
+      // Handle errors
+      catchError((error: HttpErrorResponse) => {
+        // TODO: Add error handling logic here
+        alert(`HTTP Error: ${req.url}`);
+        return throwError(error);
+      }),
+
+      // PROFILING
+      finalize(() => {
+        const profilingMsg = `${req.method} "${req.urlWithParams}"`;
+        console.log(profilingMsg);
+      })
+      );
+  };
 }
